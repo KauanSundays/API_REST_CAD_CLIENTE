9406bd3d35f2076c0d75eea62b54f22ba278b20b
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-08-28 21:00:38 -0300

    feat: "API CUSTOM"

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ClienteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/clientes', [ClienteController::class, 'index']); //Rota principal
+Route::post('/clientes', [ClienteController::class, 'store']); //Envia para o BD
+Route::get('clientes', [ClienteController::class, 'show']); // Trazer informações do BD
