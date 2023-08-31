9d19814a39fc475a118ba48f01a9e57abf1cedec
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-08-30 22:52:33 -0300

    feat: "WIP"

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use Illuminate\Support\Facades\Http;
+use App\http\Controllers\apiController;
 
 Route::get('/', function () {
-    return view('api');
+
+    $pai = Http::get('https://api.github.com/users/robertobarross/repos');
+    $apiArray = $pai->json();
+    return view('api', ['apiArray' => $apiArray]);
 });
